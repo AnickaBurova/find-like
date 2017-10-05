07900e22bc696d8ce6f4b8bda0eeef86c0475ae7
Anicka Burova <anicka.burova@gmail.com>
2017-10-05 08:08:29 +0100

    0.1.0.0: Added project information.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+/**
+ * File: src/lib.rs
+ * Author: Anicka Burova <anicka.burova@gmail.com>
+ * Date: 05.10.2017
+ * Last Modified Date: 05.10.2017
+ * Last Modified By: Anicka Burova <anicka.burova@gmail.com>
+ */
+extern crate rustkell;
+
 #[cfg(test)]
 mod tests {
     #[test]
